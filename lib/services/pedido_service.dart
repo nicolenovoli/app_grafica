267b840f8476f38e6ed675840ccd35af2dfc7d9e
Bianca Leticia <bianca_neckel@hotmail.com>
2026-06-06 21:21:12 -0300

    Feat envio por email

--- lib/services/pedido_service.dart
+++ lib/services/pedido_service.dart
@@ -7,7 +7,7 @@ import '../models/pedido_model.dart';
 class PedidoService {
 
   final String baseUrl =
-      "http://10.0.2.2:8000";
+      "http://127.0.0.1:8000";
 
   Future<PedidoModel?> criarPedido(
     PedidoModel novoPedido,
